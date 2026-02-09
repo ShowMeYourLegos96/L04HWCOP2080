7115ce447f024fea5434b182eb63d7108ec07678
Christopher Ponds <87038488+ShowMeYourLegos96@users.noreply.github.com>
2026-02-09 19:29:31 +0000

    Changed functions in the function files

--- grade_compute.py
+++ grade_compute.py
@@ -28,25 +28,25 @@ def GradeToNumber(x):
 def NumberToGrade(x):
     if x == 4:
         x = 'A+'
-    elif x == 4:
+    elif x < 4 and x > 3.7:
         x = 'A'
-    elif x == 3.7:
+    elif x <= 3.7 and x >3.3 :
         x = 'A-'
-    elif x == '3.3':
+    elif x <= 3.3 and x > 3:
         x = 'B+'
-    elif x == 3:
+    elif x <= 3 and x > 2.7:
         x = 'B'
-    elif x == 2.7:
+    elif x <= 2.7 and x > 2.3:
         x = 'B-'
-    elif x == 2.3:
+    elif x <= 2.3 and x > 2:
         x = 'C+'
-    elif x == 2:
+    elif x <= 2 and x > 1.7:
         x = 'C'
-    elif x == 1.7:
+    elif x <= 1.7 and x > 1.3:
         x = 'C-'
-    elif x == 1.3:
+    elif x <= 1.3 and x > 1:
         x = 'D+'
-    elif x == 1:
+    elif x <= 1 and x > 0:
         x = 'D'
     elif x == 0:
         x = 'F'
